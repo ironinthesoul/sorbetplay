d5714aacade34419f67c660eccb72294cbb7bcc4
Michael Townshend <michael.townshend@rsaweb.net>
2024-08-31 10:20:46 +0200

    Theme and style updates

---
+++ wp-content/themes/storefront-child/functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Storefront Child Theme Functions
+ *
+ * When running a child theme (see http://codex.wordpress.org/Theme_Development
+ * and http://codex.wordpress.org/Child_Themes), you can override certain
+ * functions (those wrapped in a function_exists() call) by defining them first
+ * in your child theme's functions.php file. The child theme's functions.php
+ * file is included before the parent theme's file, so the child theme
+ * functions will be used.
+ *
+ * Text Domain: storefront
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ */
+
+/**
+ * Load the parent style.css file
+ *
+ * @link http://codex.wordpress.org/Child_Themes
+ */
+<<<<<<< HEAD:wp-content/themes/storefront-child/functions.php
+function storefront_child_enqueue_parent_style() {
+=======
+
+
+function oceanwp_child_enqueue_parent_style() {
+>>>>>>> edee4dc6a2fd223030a1a229ecd6e18957e3e9a9:wp-content/themes/oceanwp-child-theme-master/functions.php
+
+	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
+	$theme   = wp_get_theme( 'Storefront' );
+	$version = $theme->get( 'Version' );
+
+	// Load the stylesheet.
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'storefront-style' ), $version );
+	
+}
+
+add_action( 'wp_enqueue_scripts', 'storefront_child_enqueue_parent_style' );
